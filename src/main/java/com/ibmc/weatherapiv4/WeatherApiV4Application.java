a02c9745285c673bd083a138ddbcf89e41394622
BinduSK <bindusajithkumar01@gmail.com>
2022-07-27 06:33:48 +0530

    first commit

---
+++ src/main/java/com/ibmc/weatherapiv4/WeatherApiV4Application.java
@@ -0,0 +1,13 @@
+package com.ibmc.weatherapiv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiV4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApiV4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibmc/weatherapiv4/WeatherApiV4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibmc.weatherapiv4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiV4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
